a5036a7a9b99963186992a592bed1cb8e143a794
greco <ag@architech.ca>
2014-03-30 11:41:13 -0400

    fix ui

--- js/gestureui.js
+++ js/gestureui.js
@@ -4,22 +4,28 @@ var originalTop;
 var originalLeft;
 
 var openedTile;
-  
+
 function openTile(positionDesc) {
                       
     var $this = $("#" + positionDesc);
   
     if ($this.hasClass('doNotOpen')) {
+        
+        if ($this != null && openedTile != null && $this.data('side') != openedTile.data('side')) {
+            closeOpenTile();
+        }
+        
         return
     }
   
     if (originalWidth == null && originalHeight == null) {
         originalHeight = $this.height();
         originalWidth = $this.width();
-        originalTop = $this.position().top;
-        originalLeft = $this.position().left;
     }
   
+    originalTop = $this.position().top;
+    originalLeft = $this.position().left;
+
     $(".tileVertical").each(function(){
         $(this).addClass('doNotOpen');
         $(this).css('z-index',0);
@@ -33,7 +39,8 @@ function openTile(positionDesc) {
     $this.css('z-index',1);
     if ($this.data('side') == 'left') {
         $this.animate({height:'768px', width:'1024px'}
-            //complete
+                      
+        //complete
         , function(){
             openedTile = $this;
             if ($this.data('side') == 'left') {
@@ -46,7 +53,8 @@ function openTile(positionDesc) {
     }
     else if ($this.data('side') == 'right') {
         $this.animate({height:'768px', width:'1024px', top: '0', left: '0'}
-            //complete
+                      
+        //complete
         , function(){
             openedTile = $this;
             if ($this.data('side') == 'left') {
@@ -66,6 +74,10 @@ function closeOpenTile() {
     if (isTileClosed == true) {
         return;
     }
+    
+    if (openedTile == null) {
+        return;
+    }
   
     openedTile.animate({height:originalHeight, width:originalWidth, top: originalTop, left: originalLeft}
 
@@ -74,6 +86,10 @@ function closeOpenTile() {
          
             $this = $(this)
             
+            if (openedTile == null) {
+                return;
+            }
+                       
             if ($this.data('side') == 'left') {
                 openedTile.css("background-image", "url('../images/handWaveRight.png')");
                     $(".tileVertical").each(function(){
